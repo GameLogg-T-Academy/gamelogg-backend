791567227b65ce084191e743a4611a19aae8874c
igorperonico <igormtpr@gmail.com>
2024-08-23 18:17:53 -0300

    feat: update GameController

--- src/main/java/com/gamelogg/controllers/GameController.java
+++ src/main/java/com/gamelogg/controllers/GameController.java
@@ -1,9 +1,15 @@
 package com.gamelogg.controllers;
 
+import com.gamelogg.models.Game;
 import com.gamelogg.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/games")
@@ -11,4 +17,51 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<Page<Game>> getGames(
+            @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "status", required = false) String status,
+            @RequestParam(value = "favorite", required = false) Boolean favorite,
+            @RequestParam(value = "durationMin", required = false) Integer durationMin,
+            @RequestParam(value = "durationMax", required = false) Integer durationMax,
+            @RequestParam(value = "developer", required = false) String developer,
+            @RequestParam(value = "publisher", required = false) String publisher,
+            @RequestParam(value = "releasedAfter", required = false) LocalDate releasedAfter,
+            @RequestParam(value = "releasedBefore", required = false) LocalDate releasedBefore
+    ) {
+
+        Page<Game> games = gameService.findGames(pageNumber, pageSize, title, status,
+                favorite, durationMin, durationMax, developer, publisher, releasedAfter, releasedBefore);
+
+        return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGame(@PathVariable(value = "id") Long id) {
+        Game game = gameService.findGame(id);
+        return new ResponseEntity<>(game, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> createGame(@RequestBody Game game) {
+        Game createdGame = gameService.saveGame(game);
+        return new ResponseEntity<>(createdGame, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game game) {
+        game.setId(id);
+        Game updatedGame = gameService.saveGame(game);
+        return new ResponseEntity<>(updatedGame, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long id) {
+        gameService.deleteGame(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
