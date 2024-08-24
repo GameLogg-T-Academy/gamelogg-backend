7a78ed64b3d63aa728428adb500d8ef50362375b
Paulo Henrique <ph.1928@hotmail.com>
2024-08-23 22:09:58 -0300

    chore: add endpoint summaries

--- src/main/java/com/gamelogg/controllers/GameController.java
+++ src/main/java/com/gamelogg/controllers/GameController.java
@@ -2,6 +2,7 @@ package com.gamelogg.controllers;
 
 import com.gamelogg.models.Game;
 import com.gamelogg.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,6 +18,7 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @Operation(summary = "Get list of games")
     @GetMapping
     public ResponseEntity<Page<Game>> getGames(
             @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
@@ -36,18 +38,21 @@ public class GameController {
         return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get game by ID")
     @GetMapping("/{id}")
     public ResponseEntity<Game> getGame(@PathVariable(value = "id") Long id) {
         Game game = gameService.findGame(id);
         return new ResponseEntity<>(game, HttpStatus.OK);
     }
 
+    @Operation(summary = "Add new game")
     @PostMapping
     public ResponseEntity<Game> createGame(@RequestBody Game game) {
         Game createdGame = gameService.saveGame(game);
         return new ResponseEntity<>(createdGame, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update game")
     @PutMapping("/{id}")
     public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game game) {
         game.setId(id);
@@ -55,6 +60,7 @@ public class GameController {
         return new ResponseEntity<>(updatedGame, HttpStatus.OK);
     }
 
+    @Operation(summary = "Remove game")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGame(@PathVariable Long id) {
         gameService.deleteGame(id);
