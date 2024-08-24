97ef3f26e4d4b4182b6e15fb237f7596574fb2d6
Paulo Henrique <ph.1928@hotmail.com>
2024-08-23 21:58:39 -0300

    fix: tag in game controller

--- src/main/java/com/gamelogg/controllers/GameController.java
+++ src/main/java/com/gamelogg/controllers/GameController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/games")
-@Tag(name = "GameLogg API")
+@Tag(name = "Games")
 public class GameController {
 
     @Autowired
