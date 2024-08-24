84310e12f4b8b5161dde5a836307979370f2b6f4
Paulo Henrique <ph.1928@hotmail.com>
2024-08-23 22:22:41 -0300

    refactor: adjust exception handler class name

--- src/main/java/com/gamelogg/config/ControllerExceptionHandler.java
+++ src/main/java/com/gamelogg/config/ControllerExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerExceptionHander {
+public class ControllerExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ExceptionDTO> threatDuplicateEntry(DataIntegrityViolationException exception) {
